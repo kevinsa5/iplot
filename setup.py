747f822d09a6d1d8e0b83e61e0dfb19b74c75c87
Kevin Anderson <kevinanderso@gmail.com>
2018-06-16 14:58:07 -0400

    made repo pip-installable

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'iplot',
+    version = '0.0.1',
+    url ='https://github.com/kevinsa5/iplot',
+    author = 'Kevin Anderson',
+    author_email = 'kevinanderso@gmail.com',
+    description = 'An interactive tool intended for timeseries data exploration',
+    packages = ['iplot'],    
+    install_requires = ['bokeh==0.12.16', 'pandas >= 0.23.1', 'matplotlib >= 2.0.0'],
+)
+
